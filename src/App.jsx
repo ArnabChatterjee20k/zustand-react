129e96655bf2c7c745394a10c94dbde70b9a6ad5
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-06 18:31:33 +0530

    Component For adding course

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import "./App.css"
+import CourseForm from './components/CourseForm'
 
 function App() {
   return (
-    <div>App</div>
+    <CourseForm/>
   )
 }
 
---
+++ src/components/CourseForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import useCourseStore from "../app/courseStore";
+
+export default function CourseForm() {
+  const [courseTitle, setCourseTitle] = useState("");
+  const addCourse = useCourseStore((state) => state.addCourse);
+  console.log("CourseForm Rendered");
+
+  const handleSubmit = () => {
+    addCourse({
+      id: Math.ceil(Math.random() * 1000),
+      title: courseTitle,
+    });
+  };
+
+  return (
+    <div className="form-container">
+      <input
+        type="text"
+        className="form-input"
+        value={courseTitle}
+        onChange={(e) => setCourseTitle(e.target.value)}
+      />
+      <button className="form-submit-btn" onClick={() => {
+            handleSubmit();
+        }}>
+        Add
+      </button>
+    </div>
+  );
+}
